bc4bfe88aa87ce3ac19d4a3d7cfd2d57307408ec
Julie Chung <juliehchung96@gmail.com>
2022-05-06 00:00:29 -0700

    fix: update return type

--- packages/backend/src/controllers/user.controller.ts
+++ packages/backend/src/controllers/user.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import asyncHandler from 'express-async-handler';
-import { IUser } from '../models/user';
+import { ReturnUser } from '../models/user';
 import * as userService from '../services/user.service';
 import { StatusCode } from '../types/common';
 
@@ -10,26 +10,26 @@ export const getUserList = asyncHandler(async (req: Request, res: Response): Pro
 });
 
 export const getUser = asyncHandler(async (req: Request, res: Response): Promise<void> => {
-  const user: IUser = await userService.getUser(req.params.id);
+  const user: ReturnUser = await userService.getUser(req.params.id);
   res.status(StatusCode.success).send(user);
 });
 
 export const getSelf = asyncHandler(async (req: Request, res: Response): Promise<void> => {
-  const user: IUser = await userService.getUser(res.locals.user.id);
+  const user: ReturnUser = await userService.getUser(res.locals.user.id);
   res.status(StatusCode.success).send(user);
 });
 
 export const createUser = asyncHandler(async (req: Request, res: Response): Promise<void> => {
-  const createdUser: IUser = await userService.createUser(req.body);
+  const createdUser: ReturnUser = await userService.createUser(req.body);
   res.status(StatusCode.success).send(createdUser);
 });
 
 export const updateUser = asyncHandler(async (req: Request, res: Response): Promise<void> => {
-  const updatedUser: IUser = await userService.updateUser(req.params.id, req.body);
+  const updatedUser: ReturnUser = await userService.updateUser(req.params.id, req.body);
   res.status(StatusCode.success).send(updatedUser);
 });
 
 export const deleteUser = asyncHandler(async (req: Request, res: Response): Promise<void> => {
-  const deletedUser: IUser = await userService.deleteUser(req.params.id);
+  const deletedUser: ReturnUser = await userService.deleteUser(req.params.id);
   res.status(StatusCode.success).send(deletedUser);
 });
