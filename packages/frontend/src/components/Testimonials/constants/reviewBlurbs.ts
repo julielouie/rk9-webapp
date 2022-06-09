8592cdc0da16ce2a1dd4720a179207fb5092f2a9
Julie Chung <juliehchung96@gmail.com>
2022-06-09 16:54:00 -0700

    feat: fully functional testimonials view

---
+++ packages/frontend/src/components/Testimonials/constants/reviewBlurbs.ts
@@ -0,0 +1,56 @@
+export const reviewBlurbs: { name: string; description: string; image: string }[] = [
+  {
+    name: 'Nicole & Chloe',
+    image: 'https://storage.googleapis.com/rk9-development/Nicole%20and%20Chloe.png',
+    description:
+      'Thank you Allie. For guiding us, supporting us, and for training me in how to train Chloe. I could not have done this without you.',
+  },
+  {
+    name: 'Suzy & Malibu',
+    image: 'https://storage.googleapis.com/rk9-development/Suzy%20and%20Malibu.jpg',
+    description:
+      'There is comfort knowing that Allie is straight forward and wants the best for us and our pup. I tell my friends that I am going to training, not my puppy.',
+  },
+  {
+    name: 'Lisa & Sammy',
+    image: 'https://storage.googleapis.com/rk9-development/Lisa%20and%20Sammy.png',
+    description:
+      'Sammi is a happy confident one year old Shepherd Heeler mix that will never have to be stuck in the yard or worse because of her aggression. Thank You Allie, for everything!',
+  },
+  {
+    name: 'The Frank Family',
+    image: 'https://storage.googleapis.com/rk9-development/The%20Frank%20Family.png',
+    description:
+      "I can't get over how easy it was to work with Allie... Our family is so grateful!!",
+  },
+  {
+    name: 'Hannah & Tanner',
+    image: 'https://storage.googleapis.com/rk9-development/Hannah%20and%20Tanner.png',
+    description:
+      'Allie has improved my relationship with Tanner more than I thought was possible.. I can’t even begin to imagine what my life and Tanner’s life would be like right now if it weren’t for her guidance.',
+  },
+  {
+    name: 'Kelly & Hiro',
+    image: 'https://storage.googleapis.com/rk9-development/Kelly%20and%20Hiro.jpeg',
+    description:
+      'She saved our pack when we felt desperately lost, torn on the decision to surrender Hiro to a rescue. I am forever grateful to have crossed paths with her. THANK YOU ALLIE!!!!',
+  },
+  {
+    name: 'Isella & Oreo',
+    image: 'https://storage.googleapis.com/rk9-development/Isella%20and%20Oreo.jpeg',
+    description:
+      'One thing that stood out to me and speaks volumes is that from the first training session she has included the entire family.',
+  },
+  {
+    name: 'S. & Maverick',
+    image: 'https://storage.googleapis.com/rk9-development/S.%20and%20Maverick.jpg',
+    description:
+      'I went through 4 trainers in a year’s time. Then, I was referred to Allie. This is the best training available in my opinion, especially for a WORKING DOG. There needs to be mutual respect, not fear.',
+  },
+  {
+    name: 'Alexis & Chewy',
+    image: 'https://storage.googleapis.com/rk9-development/Alexis%20and%20Chewy.jpg',
+    description:
+      'Allie is a foundation style dog trainer, which means she focuses on the handler and canine’s relationship. I really appreciate this because she doesn’t try to change who Chewy is.',
+  },
+];
--- packages/frontend/src/components/Testimonials/index.tsx
+++ packages/frontend/src/components/Testimonials/index.tsx
@@ -1,11 +1,15 @@
 import React, { FC, useState, useContext } from 'react';
-import { Typography, Grid, Button } from '@material-ui/core';
+import { Typography, Grid, Button, Box } from '@material-ui/core';
+import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
+import Carousel from 'react-material-ui-carousel';
 import useSWR from 'swr';
 import palette from '../../theme/palette';
 import { SessionContext } from '../../context/SessionContext';
 import AddOrEditTestimonial from './AddOrEditTestimonial';
 import TestimonialCard from './TestimonialCard';
 import { Testimonial } from '../../types/Testimonial';
+import { reviewBlurbs } from './constants/reviewBlurbs';
+import PawPrint from '../../assets/images/paw.png';
 
 export const Testimonials: FC = () => {
   const [openAddOrEditDialog, setOpenAddOrEditDialog] = useState(false);
@@ -35,6 +39,67 @@ export const Testimonials: FC = () => {
         </Grid>
         <Grid
           item
+          style={{
+            width: '100%',
+            backgroundColor: palette.paper.tertiary,
+            paddingBottom: '20px',
+          }}
+        >
+          <Carousel>
+            {reviewBlurbs.map((reviewBlurb) => (
+              <Box
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+                key={reviewBlurb.name}
+              >
+                <Box
+                  style={{
+                    width: '60%',
+                    height: '50vh',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'start',
+                    marginLeft: '100px',
+                  }}
+                >
+                  <Typography variant="h2">{reviewBlurb.name}</Typography>
+                  <Box style={{ display: 'flex', width: '90%' }}>
+                    <FormatQuoteIcon
+                      style={{
+                        color: palette.button.primary,
+                        transform: 'scale(-1, 1)',
+                        fontSize: '5rem',
+                      }}
+                    />
+                    <Typography variant="h5" style={{ alignSelf: 'end', fontStyle: 'italic' }}>
+                      {reviewBlurb.description}
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box style={{ display: 'flex', width: '40%', marginTop: '40px' }}>
+                  <div
+                    style={{
+                      backgroundColor: palette.disabled,
+                      backgroundImage: `url(${reviewBlurb.image})`,
+                      backgroundRepeat: 'no-repeat',
+                      backgroundSize: 'cover',
+                      borderRadius: '250px',
+                      height: '450px',
+                      width: '450px',
+                    }}
+                  />
+                </Box>
+              </Box>
+            ))}
+          </Carousel>
+        </Grid>
+        <Grid
+          item
           container
           style={{ padding: '50px 50px 0 50px', display: 'flex', justifyContent: 'end' }}
         >
@@ -50,7 +115,9 @@ export const Testimonials: FC = () => {
       </Grid>
       <Grid item container style={{ paddingBottom: '50px', display: 'flex' }}>
         {testimonials &&
-          testimonials.map((testimonial) => <TestimonialCard testimonial={testimonial} />)}
+          testimonials.map((testimonial) => (
+            <TestimonialCard key={testimonial.id} testimonial={testimonial} />
+          ))}
       </Grid>
       <AddOrEditTestimonial
         open={openAddOrEditDialog}
