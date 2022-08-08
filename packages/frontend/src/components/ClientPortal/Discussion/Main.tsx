51eb4fc6bc661101217b8653b581c8fe19525449
Julie Chung <juliehchung96@gmail.com>
2022-08-08 11:45:28 -0700

    fix: style updates

--- packages/frontend/src/components/ClientPortal/Discussion/Main.tsx
+++ packages/frontend/src/components/ClientPortal/Discussion/Main.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Box } from '@material-ui/core';
 import useSWR from 'swr';
 import NewPost from '../NewPost';
 import ReadPost from '../ReadPost';
@@ -20,11 +20,26 @@ export const Main: FC<MainProps> = (props) => {
 
   return (
     <Grid container>
-      <Grid item xs={12} style={{ marginBottom: '30px' }}>
-        <NewPost groupInfo={groupInfo} updatePath={path} />
-        {allGroupPosts &&
-          allGroupPosts.length &&
-          allGroupPosts.map((post) => <ReadPost key={post.id} post={post} />)}
+      <Grid
+        item
+        xs={12}
+        style={{
+          marginBottom: '30px',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Box
+          style={{
+            width: '60%',
+          }}
+        >
+          <NewPost groupInfo={groupInfo} updatePath={path} />
+          {allGroupPosts &&
+            allGroupPosts.length &&
+            allGroupPosts.map((post) => <ReadPost key={post.id} post={post} />)}
+        </Box>
       </Grid>
     </Grid>
   );
