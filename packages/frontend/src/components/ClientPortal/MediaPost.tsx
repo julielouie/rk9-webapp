1f201d327c77c02e3a762afa18ad1caaf660e159
Julie Chung <juliehchung96@gmail.com>
2022-08-08 11:33:45 -0700

    feat: working styled discussion tab

--- packages/frontend/src/components/ClientPortal/MediaPost.tsx
+++ packages/frontend/src/components/ClientPortal/MediaPost.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from 'react';
-import { Card, CardContent } from '@material-ui/core';
+import React, { FC, useState } from 'react';
+import { Card, CardContent, Dialog, Box, IconButton } from '@material-ui/core';
+import CloseIcon from '@mui/icons-material/Close';
 import { Post } from '../../types/Post';
 
 interface MediaPostProps {
@@ -8,20 +9,66 @@ interface MediaPostProps {
 
 export const MediaPost: FC<MediaPostProps> = (props) => {
   const { post } = props;
+  const [openExpanded, setOpenExpanded] = useState(false);
 
   return (
-    <Card
-      variant="outlined"
-      style={{
-        display: 'inline-block',
-        verticalAlign: 'top',
-        cursor: 'pointer',
-        margin: '20px 20px 0 0',
-      }}
-    >
-      <CardContent style={{ padding: '15px', maxWidth: '350px' }}>
+    <>
+      <Card
+        variant="outlined"
+        style={{
+          display: 'inline-block',
+          verticalAlign: 'top',
+          cursor: 'pointer',
+          margin: '20px 20px 0 0',
+        }}
+        onClick={() => setOpenExpanded(true)}
+      >
+        <CardContent style={{ padding: '15px', maxWidth: '350px' }}>
+          {post.media && (
+            <div style={{ maxWidth: '400px' }}>
+              {post.mediaType === 'photo' && (
+                <img alt="post-media" src={post.media} style={{ height: '100%', width: '100%' }} />
+              )}
+              {post.mediaType === 'video' && (
+                <video width="100%">
+                  <source src={post.media} />
+                  <track default kind="captions" srcLang="en" />
+                </video>
+              )}
+            </div>
+          )}
+        </CardContent>
+      </Card>
+      <Dialog
+        open={openExpanded}
+        onClose={() => setOpenExpanded(false)}
+        fullWidth
+        maxWidth="xl"
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Box
+          style={{
+            display: 'flex',
+            justifyContent: 'end',
+            alignItems: 'center',
+            paddingTop: '15px',
+          }}
+        >
+          <IconButton onClick={() => setOpenExpanded(false)}>
+            <CloseIcon />
+          </IconButton>
+        </Box>
         {post.media && (
-          <div style={{ maxWidth: '400px' }}>
+          <div
+            style={{
+              maxWidth: '50vw',
+              padding: '15px',
+            }}
+          >
             {post.mediaType === 'photo' && (
               <img alt="post-media" src={post.media} style={{ height: '100%', width: '100%' }} />
             )}
@@ -33,8 +80,8 @@ export const MediaPost: FC<MediaPostProps> = (props) => {
             )}
           </div>
         )}
-      </CardContent>
-    </Card>
+      </Dialog>
+    </>
   );
 };
 
--- packages/frontend/src/components/ClientPortal/ReadPost.tsx
+++ packages/frontend/src/components/ClientPortal/ReadPost.tsx
@@ -1,6 +1,7 @@
-import React, { FC } from 'react';
-import { Typography, Grid, Card, CardContent, Box } from '@material-ui/core';
+import React, { FC, useState } from 'react';
+import { Typography, Grid, Card, CardContent, Box, Dialog, IconButton } from '@material-ui/core';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import CloseIcon from '@mui/icons-material/Close';
 import dayjs from 'dayjs';
 import palette from '../../theme/palette';
 import { Post } from '../../types/Post';
@@ -11,60 +12,124 @@ interface ReadPostProps {
 
 export const ReadPost: FC<ReadPostProps> = (props) => {
   const { post } = props;
+  const [openExpanded, setOpenExpanded] = useState(false);
 
   return (
-    <Grid container>
-      <Grid item xs={12}>
-        <Card variant="outlined" style={{ marginTop: '40px' }}>
-          <CardContent
-            style={{
-              marginTop: '10px',
-            }}
-          >
-            <Box
+    <>
+      <Grid container>
+        <Grid item xs={12}>
+          <Card variant="outlined" style={{ marginTop: '40px' }}>
+            <CardContent
               style={{
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
+                marginTop: '10px',
               }}
             >
-              <Typography
+              <Box
                 style={{
                   display: 'flex',
+                  flexDirection: 'row',
                   alignItems: 'center',
                 }}
               >
-                <AccountCircleIcon
-                  style={{ fontSize: '3rem', color: palette.text.primary, marginRight: '15px' }}
-                />
-              </Typography>
-              <Typography style={{ display: 'flex', flexDirection: 'column' }}>
-                <span>{post.user.name}</span>
-                <span>{dayjs(post.date).format('MMM D, YYYY - h:mma')}</span>
-              </Typography>
-            </Box>
-            {post.text && <Box style={{ marginTop: '25px' }}>{post.text}</Box>}
-            {post.media && (
-              <div style={{ marginTop: '15px', maxWidth: '400px' }}>
-                {post.mediaType === 'photo' && (
-                  <img
-                    alt="post-media"
-                    src={post.media}
-                    style={{ height: '100%', width: '100%' }}
+                <Typography
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                  }}
+                >
+                  <AccountCircleIcon
+                    style={{ fontSize: '3rem', color: palette.text.primary, marginRight: '15px' }}
                   />
-                )}
-                {post.mediaType === 'video' && (
-                  <video width="100%" controls>
-                    <source src={post.media} />
-                    <track default kind="captions" srcLang="en" />
-                  </video>
-                )}
-              </div>
-            )}
-          </CardContent>
-        </Card>
+                </Typography>
+                <Typography style={{ display: 'flex', flexDirection: 'column' }}>
+                  <span>{post.user.name}</span>
+                  <span>{dayjs(post.date).format('MMM D, YYYY - h:mma')}</span>
+                </Typography>
+              </Box>
+              {post.text && <Box style={{ marginTop: '25px' }}>{post.text}</Box>}
+              {post.media && (
+                <Card
+                  variant="outlined"
+                  style={{
+                    display: 'inline-block',
+                    border: 'none',
+                    verticalAlign: 'top',
+                    cursor: 'pointer',
+                    margin: '20px 20px 0 0',
+                    padding: 0,
+                  }}
+                  onClick={() => setOpenExpanded(true)}
+                >
+                  <CardContent
+                    style={{
+                      maxWidth: '400px',
+                      cursor: 'pointer',
+                      padding: 0,
+                    }}
+                  >
+                    {post.mediaType === 'photo' && (
+                      <img
+                        alt="post-media"
+                        src={post.media}
+                        style={{ borderRadius: '5px', height: '100%', width: '100%' }}
+                      />
+                    )}
+                    {post.mediaType === 'video' && (
+                      <video width="100%" style={{ borderRadius: '5px' }}>
+                        <source src={post.media} />
+                        <track default kind="captions" srcLang="en" />
+                      </video>
+                    )}
+                  </CardContent>
+                </Card>
+              )}
+            </CardContent>
+          </Card>
+        </Grid>
       </Grid>
-    </Grid>
+      <Dialog
+        open={openExpanded}
+        onClose={() => setOpenExpanded(false)}
+        fullWidth
+        maxWidth="xl"
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Box
+          style={{
+            display: 'flex',
+            justifyContent: 'end',
+            alignItems: 'center',
+            paddingTop: '15px',
+          }}
+        >
+          <IconButton onClick={() => setOpenExpanded(false)}>
+            <CloseIcon />
+          </IconButton>
+        </Box>
+        {post.media && (
+          <div
+            style={{
+              maxWidth: '50vw',
+              padding: '15px',
+            }}
+          >
+            {post.mediaType === 'photo' && (
+              <img alt="post-media" src={post.media} style={{ height: '100%', width: '100%' }} />
+            )}
+            {post.mediaType === 'video' && (
+              <video width="100%" controls>
+                <source src={post.media} />
+                <track default kind="captions" srcLang="en" />
+              </video>
+            )}
+          </div>
+        )}
+      </Dialog>
+    </>
   );
 };
 
