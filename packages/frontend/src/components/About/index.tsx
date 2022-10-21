451a0be818066540499b578c4d4de58514db27f4
Julie Chung <juliehchung96@gmail.com>
2022-10-21 15:52:47 -0700

    fix: about me wording change

--- packages/frontend/src/components/About/index.tsx
+++ packages/frontend/src/components/About/index.tsx
@@ -89,8 +89,8 @@ export const About: FC = () => {
         </Grid>
         <Grid item md={12}>
           <Typography variant="h5" style={{ padding: '50px' }}>
-            My passion is working with strong temperament dogs, and the people who love them enough
-            to help them have the best possible life.
+            My passion is working with strong temperament and aggressive dogs, and the people who
+            love them enough to help them have the best possible life.
           </Typography>
         </Grid>
         <Grid
